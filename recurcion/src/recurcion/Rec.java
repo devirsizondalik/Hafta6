84993c713019f707883b6ea9055138a69cdc6fc1
Lenovo <Lenovo@10.48.105.203>
2022-11-10 09:01:55 +0300

    Rec

---
+++ recurcion/src/recurcion/Rec.java
@@ -0,0 +1,27 @@
+package recurcion;
+
+import java.time.LocalDateTime;
+
+public class Rec {
+	static int n1=0, n2=1, n3=0;
+	static void fib(int count) 
+	{
+		if(count>0) 
+		{
+			n3=n1+n2;
+			n1=n2;
+			n2=n3;
+			System.out.print(" " + n3);
+			fib(count-1);
+		}
+	}
+		public static void main(String[] args) {
+			int count=10;
+			LocalDateTime time=LocalDateTime.now(); 
+			System.out.print(n1+" "+n2);
+			fib(count-2);
+			System.out.println(" ");
+			System.out.println(time);
+	}
+
+}
