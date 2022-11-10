9669482a6a1b4c6dea1cb2ab98de632400b9704a
Lenovo <Lenovo@10.48.105.203>
2022-11-10 09:32:20 +0300

    Rec,HashMap

--- recurcion/src/recurcion/Hashmap.java
+++ recurcion/src/recurcion/Hashmap.java
@@ -1,10 +1,12 @@
 package recurcion;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Hashmap {
 
 	public static void main(String[] args) {
+		LinkedList<String> cars=new LinkedList<String>();
 		HashMap<String, Integer> people=new HashMap<String,Integer>();
 		people.put("Emre", 21);
 		people.put("Ahmet", 30);
@@ -13,7 +15,21 @@ public class Hashmap {
 		{
 			System.out.println("key : "+ i+ "\nValue : "+people.get(i));
 		}
-
+		cars.add("Volvo");
+		cars.add("Mercedes");
+		cars.add("Ford");
+		cars.add(1,"Mazda");
+		String a=cars.get(1);
+		if(a=="Mercedes") 
+		{
+			System.out.println("Eşitlik Sağlandı...");
+		}
+		else if(a.equals(cars.get(1))) 
+		{
+			System.out.println("Equals Sağlandı...");
+		}
+		else
+			System.out.println("Eşitlik Sağlanamadı");
 	}
 
 }
